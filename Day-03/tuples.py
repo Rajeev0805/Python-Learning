e6ce95f84dfb79e8ddc1c1effefd646cd8ec7a0c
Rajeev0805 <rajeevgupta0805@gmail.com>
2026-07-07 23:13:01 +0530

    Day 3 is still pending

---
+++ Day-03/tuples.py
@@ -0,0 +1,15 @@
+coordinates = (12.5,78.6)
+
+latitude,longitude = coordinates
+print("latitude : ",latitude)
+print("longitude : ",longitude)
+
+student = ("Rajeev",22,"IIIT")
+name,age,college = student
+print("Name : ", name)
+print("Age : ", age)
+print("college : ", college)
+
+numbers = (10,20,30,20,40)
+print("count of 20 : ",  numbers.count(20))
+print("Index of 30 : " , numbers.index(30))
